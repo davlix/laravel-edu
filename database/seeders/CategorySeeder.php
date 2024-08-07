f6ebea46425f5955336324a5e989b693d3a6d50f
Davlix <dhema112@gmail.com>
2024-08-07 17:28:38 +0700

    update all
    update main

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['frameworks', 'php', 'javascript', 'python'];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+            ]);
+        }
+    }
+}
---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,40 @@
+@extends('adminlte::page')
+
+@section('title', config('app.name'))
+
+@section('content_header')
+    <h1>Create new Category</h1>
+@stop
+
+@section('content')
+    @if (session('info'))
+        <div class="alert alert-success" role="alert">
+            <strong>{{ session('info') }}</strong>
+        </div>
+    @endif
+
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.categories.store']) !!}
+
+            @include('admin.categories.partials.form')
+
+            {!! Form::submit('Create category', ['class' => 'btn btn-primary']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+@section('js')
+    <script src="{{ asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+        });
+    </script>
+@stop
